fc90475c0204c1a5add78519868488702436253f
leemurking <jamesgrant964@gmail.com>
2021-06-14 12:28:55 +0300

    correction to the spelling errors

--- myscripts.js
+++ myscripts.js
@@ -1,102 +1,75 @@
-function MakeArray(n) {
-    this.length = n;
-    for (var i = 1; i <=n; i++) {
-      this[i] = 0;
+function naming(){
+    var CC=parseInt(document.getElementById("main").value);
+    var YY=parseInt(document.getElementById("name").value);
+    var MM=parseInt(document.getElementById("Form").value);
+    var DD=parseInt(document.getElementById("akan").value);
+    var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
+    var femaleNames=["Akosua", "Adwoa","Abenaa","Akua","Yaa"," Afua","Ama"];
+    var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    if(CC.value =="" || YY.value =="" || MM.value =="" || DD.value ==""){
+        alert("No blank values allowed");
     }
- }
- days = new MakeArray(7);
- days[0] = "Kwame"
- days[1] = "Kwasi"
- days[2] = "Kwadwo"
- days[3] = "Kwabena"
- days[4] = "Kwaku"
- days[5] = "Yaw"
- days[6] = "Kofi"
- day = new MakeArray(7);
- day[0] = "Ama"
- day[1] = "Akosua"
- day[2] = "Adwoa"
- day[3] = "Abenaa"
- day[4] = "Akua"
- day[5] = "Yaa"
- day[6] = "Afua"
+    var gender = document.querySelector('input[name="gender"]:checked').value;
+    var  dayOfTheWeek = parseInt( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
+    if(gender==="male") {
+      if(dayOfTheWeek===0){
+        alert("You were born on: " + day[0]);
+        alert("Your Akan name is: " + maleNames[0]);
+      } else if(dayOfTheWeek===1){
+        alert("You were born on: " + day[1]);
+        alert("Your Akan name is: " + maleNames[1]);
+      } else if(dayOfTheWeek===2){
+        alert("You were born on: " + day[2]);
+        alert("Your Akan name is: " + maleNames[2]);
+      } else if(dayOfTheWeek===3){
+        alert("You were born on: " + day[3]);
+        alert("Your Akan name is: " + maleNames[3]);
+      } else if(dayOfTheWeek===4){
+        alert("You were born on: " + day[4]);
+        alert("Your Akan name is: " + maleNames[4]);
+      } else if(dayOfTheWeek===5){
+        alert("You were born on: " + day[5]);
+        alert("Your Akan name is: " + maleNames[5]);
+      } else if(dayOfTheWeek===6){
+        alert("You were born on: " + day[6]);
+        alert("Your Akan name is: " + maleNames[6]);
+      }
+    } else {
+       if(dayOfTheWeek===0){
+        alert("You were born on: " + day[0]);
+        alert("Your Akan name is: " + femaleNames[0]);
+      } else if(dayOfTheWeek===1){
+        alert("You were born on: " + day[1]);
+        alert("Your Akan name is: " + femaleNames[1]);
+      } else if(dayOfTheWeek===2){
+        alert("You were born on: " + day[2]);
+        alert("Your Akan name is: " + femaleNames[2]);
+      } else if(dayOfTheWeek===3){
+        alert("You were born on: " + day[3]);
+        alert("Your Akan name is: " + femaleNames[3]);
+      } else if(dayOfTheWeek===4){
+        alert("You were born on: " + day[4]);
+        alert("Your Akan name is: " + femaleNames[4]);
+      } else if(dayOfTheWeek===5){
+        alert("You were born on: " + day[5]);
+        alert("Your Akan name is: " + femaleNames[5]);
+      } else if(dayOfTheWeek===6){
+        alert("You were born on: " + day[6]);
+        alert("Your Akan name is: " + femaleNames[6]);
+      }
+    }
+  }
+  
+  
 
- months = new MakeArray(12);
- months[1] = "January"
- months[2] = "February"
- months[3] = "March"
- months[4] = "April"
- months[5] = "May"
- months[6] = "June"
- months[7] = "July"
- months[8] = "August"
- months[9] = "September"
- months[10] = "October"
- months[11] = "November"
- months[12] = "December"
- 
- function compute(form) {
-    var val1 = parseInt(form.day.value, 10)
-    if ((val1 < 0) || (val1 > 31)) {
-       alert("Day is out of range")
-    }
-    var val2 = parseInt(form.month.value, 10)
-    if ((val2 < 0) || (val2 > 12)) {
-       alert("Month is out of range")
-    }
-    var val2x = parseInt(form.month.value, 10)
-    var val3 = parseInt(form.year.value, 10)
-    if (val3 < 1900) {
-       alert("You're that old!")
-    }
-    if (val2 == 1) {
-       val2x = 13;
-       val3 = val3-1
-    }
-    if (val2 == 2) {
-       val2x = 14;
-       val3 = val3-1
-    }
-    var val4 = parseInt(((val2x+1)*3)/5, 10)
-    var val5 = parseInt(val3/4, 10)
-    var val6 = parseInt(val3/100, 10)
-    var val7 = parseInt(val3/400, 10)
-    var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
-    var val9 = parseInt(val8/7, 10)
-    var val0 = val8-(val9*7)
-    form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
-    form.result2.value = days[val0]
- }
- function tomm(form) {
-    var val1 = parseInt(form.day.value, 10)
-    if ((val1 < 0) || (val1 > 31) || (val1 = null) || (val1 = '')) {
-       alert("Day is out of range")
-    }
-    var val2 = parseInt(form.month.value, 10)
-    if ((val2 < 0) || (val2 > 12) || (val1 = null) || (val1 = '')) {
-       alert("Month is out of range")
-    }
-    var val2x = parseInt(form.month.value, 10)
-    var val3 = parseInt(form.year.value, 10)
-    if (val3 < 1900) {
-       alert("You're too old!")
-    }
-    if (val2 == 1) {
-       val2x = 13;
-       val3 = val3-1
-    }
-    if (val2 == 2) {
-       val2x = 14;
-       val3 = val3-1
-    }
-    var val4 = parseInt(((val2x+1)*3)/5, 10)
-    var val5 = parseInt(val3/4, 10)
-    var val6 = parseInt(val3/100, 10)
-    var val7 = parseInt(val3/400, 10)
-    var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
-    var val9 = parseInt(val8/7, 10)
-    var val0 = val8-(val9*7)
-    form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
-    form.result2.value = day[val0]
- }
- 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
